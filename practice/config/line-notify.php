b92779c3219eab263a43e30e970afa4ffa4ef0f3
Chanave <97772054+Chanave@users.noreply.github.com>
2022-04-28 13:16:42 +0700

    Delete practice directory

--- practice/config/line-notify.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'access_token' => env('YIyR5wMAacZypMtDbjmNaZeUoGPoxejVsOlI1i5ZoqC', null),
-];
-use Phattarachai\LineNotify\Facade\Line;
-
-// จากใน Controller หรือที่อื่น ๆ
-Line::send('ทดสอบส่งข้อความ');
-
